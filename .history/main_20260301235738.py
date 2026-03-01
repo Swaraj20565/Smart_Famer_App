5024f2005edea47ee99b249a98acb7911b0b5292
Swaraj Bhumare <swarajbhumare2021@gmail.com>
2026-03-02 00:06:17 +0530

    Crop Recommendation ML + FastAPI backend

---
+++ .history/main_20260301235738.py
@@ -0,0 +1,59 @@
+# from fastapi import FastAPI
+# import joblib
+# import pandas as pd
+
+# app = FastAPI()
+
+# # Load model
+# model = joblib.load("crop_model.pkl")
+
+# @app.get("/")
+# def home():
+#     return {"message": "Crop Recommendation API Running"}
+
+# @app.post("/predict")
+# def predict(data: dict):
+
+#     sample = pd.DataFrame([[
+#         data["N"],
+#         data["P"],
+#         data["K"],
+#         data["temperature"],
+#         data["humidity"],
+#         data["ph"],
+#         data["rainfall"]
+#     ]], columns=["N","P","K","temperature","humidity","ph","rainfall"])
+
+#     prediction = model.predict(sample)
+
+#     return {"recommended_crop": prediction[0]}
+
+from fastapi import FastAPI
+import joblib
+import pandas as pd
+
+app = FastAPI()
+
+model = joblib.load("crop_model.pkl")
+
+@app.get("/")
+def home():
+    return {"message": "Crop Recommendation API Running"}
+
+@app.post("/predict")
+def predict(data: dict):  
+    # https://soilhealth.dac.gov.in/slusi-visualisation/
+
+    sample = pd.DataFrame([[
+        data["N"],
+        data["P"],
+        data["K"],
+        data["temperature"],
+        data["humidity"],
+        data["ph"],
+        data["rainfall"]
+    ]], columns=["N","P","K","temperature","humidity","ph","rainfall"])
+
+    prediction = model.predict(sample)
+
+    return {"crop": prediction[0]}
